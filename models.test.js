cec1b1b6013f3ebcc9bbb4df757196f43abc3b23
piptxt <andres_clemente@dlsu.edu.ph>
2023-07-17 16:09:53 +0800

    updated modelstest.js

--- models.test.js
+++ models.test.js
@@ -12,78 +12,86 @@ const OrdersModel = require('./models/ordersDB');
 const BlogsModel = require('./models/blogsDB');
 
 
-// TEST FOR FINDING A USER (for search)
-test('Should return the corresponding user', async () => {
-    const user_id = '6420384a21db20b7fab37534';
+describe('Tests if the mongoose DB functions are working as expected', () => {
 
-    // EXPECTED VALUE(S)
-    const ex_user = 'kennymkl';
-    const ex_usertype = 1;
-    const ex_email = "kenn@gmail.com";
-    const ex_address = "";
-    const ex_contactno = ""
+    // TEST FOR FINDING A USER (for search)
+    test('Should return the corresponding user', async () => {
+        const user_id = '64b4f17fabc5cddaad670762';
 
-    const user = await UserModel.findOne({_id: user_id});
+        // EXPECTED VALUE(S)
+        const ex_user = 'testerist';
+        const ex_usertype = 0;
+        const ex_email = "pipoclemente798@gmail.com";
+        const ex_address ={
+            addressline1: 'L15, B35',
+            addressline2: 'Mahiwaga St., Araw Araw Village',
+            city: 'Pasig',
+            region: 'Metro Manila'
+        };
+        const ex_contactno = "09177654321"
 
-    expect(String(user.username)).toBe(ex_user);
-    expect(user.user_type).toBe(ex_usertype);
-    expect(String(user.email)).toBe(ex_email);
-    expect(String(user.address)).toBe(ex_address);
-    expect(String(user.contact_no)).toBe(ex_contactno);
-});
+        const user = await UserModel.findOne({_id: user_id});
 
-//TEST FOR FILTERING USERS BY USER TYPE
-test('Should return the users with the corresponding admins', async () => {
-    const user_type = 1;
-
-    // EXPECTED VALUE(S)
-    const ex_admins = ["LebronJordan","RomanReigns", "awaw", "idolnation", "kennymkl", "pipo_clems", "testing"];
-
-    // FIND ALL ADMINS
-    const num_admins = await UserModel.find({user_type: user_type}).sort({username: 1});
-
-    let i = 0;
-    num_admins.forEach((admin) => {
-        expect(admin.username).toBe(ex_admins[i]);
-        i++;
+        expect(String(user.username)).toBe(ex_user);
+        expect(user.user_type).toBe(ex_usertype);
+        expect(String(user.email)).toBe(ex_email);
+        expect(user.address).toEqual(ex_address);
+        expect(String(user.contact_no)).toBe(ex_contactno);
     });
+
+    //TEST FOR FILTERING USERS BY USER TYPE
+    test('Should return the users with the corresponding admins', async () => {
+        const user_type = 1;
     
-});
-
-// TEST FOR FILTERING ORDERS
-test('Should return the users with the corresponding order numbers', async () => {
-    const order_status = "Out for Delivery.";
-
-    // EXPECTED VALUE(S)
-    const ex_orderids = ["6486d086b1bb788a1427cd61", "643aba197b906759e595e48b", "6437e2f3b7a56ab0fd55a423"];
- 
-    const orders = await OrdersModel.find({status: order_status}).sort({username: 1});
-    // CHECKS ALL OF THE ORDERS WITH THE CORRESPONDING STATUS
-    let i = 0;
-    orders.forEach((order) => {
-        expect(String(order._id)).toBe(ex_orderids[i]);
-        i++;
+        // EXPECTED VALUE(S)
+        const ex_admins = ["awaw", "Darren", "idolnation", "kennymkl", "LebronJordan", "pipo_clems", "RomanReigns"];
+    
+        // FIND ALL ADMINS
+        const num_admins = await UserModel.find({user_type: user_type}).sort({username: 1}).collation({locale: 'en', caseLevel: true});
+    
+        let i = 0;
+        num_admins.forEach((admin) => {
+            expect(admin.username).toBe(ex_admins[i]);
+            i++;
+        });
+        
     });
-});
 
-// TEST FOR CHECKING THE ITEMS IN THE USER'S CART
-test('Should return the users with the corresponding items of the user in their shopping cart', async () => {
-    const username = "testerist";
+    // TEST FOR FILTERING ORDERS
+    test('Should return the users with the corresponding order numbers', async () => {
+        const order_status = "Out for Delivery.";
+    
+        // EXPECTED VALUE(S)
+        const ex_orderids = ["6486d086b1bb788a1427cd61", "643aba197b906759e595e48b", "6437e2f3b7a56ab0fd55a423"];
+     
+        const orders = await OrdersModel.find({status: order_status}).sort({username: 1});
+        // CHECKS ALL OF THE ORDERS WITH THE CORRESPONDING STATUS
+        let i = 0;
+        orders.forEach((order) => {
+            expect(String(order._id)).toBe(ex_orderids[i]);
+            i++;
+        });
+    });
 
-    // EXPECTED VALUE(S)
-    const ex_items = ["NLND (Black) ", "NLND (Black) "]
-    const ex_sizes = ["L", "XS"]
-    const ex_qty = [1, 5]
-    const ex_price = 550
- 
-    const usercart = await UserCartModel.findOne({username: username})
-    // CHECKS ALL OF THE ORDERS WITH THE CORRESPONDING STATUS
-    let i = 0;
-    usercart.items.forEach((item) => {
-        expect(String(item.item_name)).toBe(ex_items[i]);
-        expect(String(item.size)).toBe(ex_sizes[i]);
-        expect(item.quantity).toBe(ex_qty[i]);
-        expect(item.price).toBe(ex_price);
-        i++;
+    // TEST FOR CHECKING THE ITEMS IN THE USER'S CART
+    test('Should return the users with the corresponding items of the user in their shopping cart', async () => {
+        const username = "testerist";
+    
+        // EXPECTED VALUE(S)
+        const ex_items = ["No Data (White) "]
+        const ex_sizes = ["S"]
+        const ex_qty = [1]
+        const ex_price = 100
+     
+        const usercart = await UserCartModel.findOne({username: username})
+        // CHECKS ALL OF THE ORDERS WITH THE CORRESPONDING STATUS
+        let i = 0;
+        usercart.items.forEach((item) => {
+            expect(String(item.item_name)).toBe(ex_items[i]);
+            expect(String(item.size)).toBe(ex_sizes[i]);
+            expect(item.quantity).toBe(ex_qty[i]);
+            expect(item.price).toBe(ex_price);
+            i++;
+        });
     });
 });
