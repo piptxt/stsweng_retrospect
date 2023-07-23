cd51a82f3f15a18590ace3caf9df6e1e2ac33dda
priscilla-licup <113849229+priscilla-licup@users.noreply.github.com>
2023-07-24 03:58:00 +0800

    Revert "Update email_notifs.test.js"
    This reverts commit aa79aadada3cda8128636d22f4243a4fd03000eb.

--- email_notifs.test.js
+++ email_notifs.test.js
@@ -4,27 +4,28 @@ const OrdersModel = require('./models/ordersDB');
 
 const expected_message = {
     to: 'priscilla_licup@dlsu.edu.ph',
-    subject: 'Payment Successful! Preparing your Order 64b4dc2d06efd9837c642b6b',
+    subject: 'Order Update: Payment Confirmation for Order 64b4dc2d06efd9837c642b6b',
     html: '\n' +
-    '                <h1 style="text-align: center;"><strong>RETROSPECT</strong></h1>\n' +
-    '                <hr />\n' +
-    '                <h3 style="text-align: center;">Payment successful! Your order is being prepared for shipment.</h3>\n' +
-    '                <hr />\n' +
-    '                <p>Hello <strong>priscilla</strong>!</p>\n' +
-    '                <p>&nbsp;</p>\n' +
-    '                <p>We have fantastic news to share! Your payment for the order has been successfully processed, and we are thrilled to inform you that we have started preparing your order for shipment.</p>\n' +
-    '                <p>&nbsp;</p>\n' +
-    '                <p><strong>Order Number: 64b5cd92b33ed1d429628822</strong></p>\n' +
-    '                <p><strong>Order Date: undefined</strong></p>\n' +
-    '                <p><strong>Total Amount: Php 100.00</strong></p>\n' +
-    '                <p>&nbsp;</p>\n' +
-    "                <p>Our dedicated team is now carefully picking, packing, and double-checking your items to ensure everything is in perfect order. We are committed to ensuring a smooth delivery process, and we'll keep you updated every step of the way. If you have any questions or need assistance, our friendly customer support team is here to help. Don't hesitate to reach out to us.</p>\n" +
-    '                <p>&nbsp;</p>\n' +
-    "                <p>Thank you for your prompt payment and for choosing us for your purchase. We appreciate your trust in our brand, and we can't wait for you to receive your order soon!</p>\n" +
-    '                <p>&nbsp;</p>\n' +
-    '                <p>Best regards,</p>\n' +
-    '                <p>Retrospect</p>\n' +
-    '                  '
+      '                  <h1 style="text-align: center;"><strong>RETROSPECT</strong></h1>\n' +
+      '                  <hr />\n' +
+      '                  <h3 style="text-align: center;">Your order is waiting for payment confirmation.</h3>\n' +
+      '                  <hr />\n' +
+      '                  <p>Hi <strong>priscilla</strong>!</p>\n' +
+      '                  <p>&nbsp;</p>\n' +
+      "                  <p>Hope you're doing well! We wanted to give you a quick heads-up about your order, <strong>64b4dc2d06efd9837c642b6b</strong>. We're just a step away from getting it ready for shipment, but we need your help with payment confirmation. Once we receive the confirmation, we'll swiftly prepare your order for shipment.</p>\n" +
+      '                  <p>&nbsp;</p>\n' +
+      '                  <p><strong>Order Number: 64b4dc2d06efd9837c642b6b</strong></p>\n' +
+      '                  <p><strong>Order Date: undefined</strong></p>\n' +
+      '                  <p><strong>Total Amount: Php 100.00</strong></p>\n' +
+      '                  <p>&nbsp;</p>\n' +
+      "                  <p>If you've already made the payment, please disregard this message. Apologies for any confusion caused. \n" +
+      '                  <p>Need assistance or have questions? Our friendly customer support team is ready to help!</p>\n' +
+      '                  <p>&nbsp;</p>\n' +
+      "                  <p>Thank you for choosing us. We appreciate your swift action in confirming the payment. We can't wait to finalize your order and provide you with an amazing shopping experience.</p>\n" +
+      '                  <p>&nbsp;</p>\n' +
+      '                  <p>Best regards,</p>\n' +
+      '                  <p>Retrospect</p>\n' +
+      '                  '
   };
 
   test('Returns the correct recipient, email subject, and message for email to be sent', async () => {
@@ -45,7 +46,6 @@ const expected_message = {
     // const user_email = user.email;
 
     const final_message = await sendEmail(user_email, order_status, newOrder);
-    console.log(final_message);
 
     expect(final_message[0]).toBe(expected_message[0]);
     expect(final_message[1]).toBe(expected_message[1]);
