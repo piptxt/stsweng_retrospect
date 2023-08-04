c897809a791f23eae6e9701a87cadf38b3313617
priscilla-licup <113849229+priscilla-licup@users.noreply.github.com>
2023-08-05 04:22:21 +0800

    Update order_history.test.js

--- order_history.test.js
+++ order_history.test.js
@@ -4,7 +4,7 @@ describe('Tests the viewing of past orders in the app', () => {
     test('Should return the corresponding past orders', async () => {
         const {past_orders, cities} = await viewPastOrders();
 
-        const act_orders = ['64b4fc7e5b5f53d347dbb676', '64b4fcb15b5f53d347dbb6c8', '64ba633367119bdc57365eb4' , '64b6628f15cdf221fe297942', '64c8dda5602dd66cf1ab2e55', '64b4faa46c4a268c326c5afe', '64ca3e233433f1dcaba5be53', '64ca3e2e3433f1dcaba5be78', '64ca3e393433f1dcaba5be9d' ];
+        const act_orders = ['64b4fc7e5b5f53d347dbb676', '64b4fcb15b5f53d347dbb6c8', '64ba633367119bdc57365eb4' , '64b6628f15cdf221fe297942', '64c8dda5602dd66cf1ab2e55', '64ca3e233433f1dcaba5be53', '64ca3e2e3433f1dcaba5be78', '64ca3e393433f1dcaba5be9d', '64b4faa46c4a268c326c5afe' ];
 
         let i = 0;
         past_orders.forEach((order) => {
